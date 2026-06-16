c8cb5131d3c9f887ddf168efe0dc4721fd4e47ed
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2026-06-16 17:25:14 +0530

    socket cookie check

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import app from "./app";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import jwt from 'jsonwebtoken';
 
 import dotenv from "dotenv";
 dotenv.config();
 
+const JWT_SECRET = process.env.JWT_SECRET;
 const PORT = process.env.PORT;
 
 
@@ -16,6 +18,25 @@ const io = new Server(httpServer, {
     }
 });
 
+io.use(async(socket, next) => {
+    try{
+        const cookie = socket.handshake.headers.cookie;
+        const tokenCookie = cookie?.split("; ").find(c => c.startsWith("token="));
+        const token = tokenCookie?.split("=")[1];
+        if(!token){
+            return next(new Error("Authentication Error"))
+        };
+
+        const decoded = jwt.verify(token as string, JWT_SECRET as string);
+        socket.data.user = decoded;
+        next();
+    }catch(err){
+        console.error(err);
+        next(new Error("Authentication Error"));
+    }
+
+});
+
 io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
 
@@ -23,6 +44,7 @@ io.on("connection", (socket) => {
         console.log("User disconnected:", socket.id);
     });
 });
+
 
 
 httpServer.listen(PORT, () => {
