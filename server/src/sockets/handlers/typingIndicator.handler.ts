2062075b1946872ea447c5ff76c625a6a1670dc7
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2026-08-05 01:45:36 +0530

    wrote server code for typing indicator

---
+++ server/src/sockets/handlers/typingIndicator.handler.ts
@@ -0,0 +1,23 @@
+import { Socket } from "socket.io";
+export default function typingHandler(socket: Socket) {
+    socket.on("conversation:typing:start", ({ conversationId }: { conversationId: string }) => {
+        const userId  = socket.data.user.id;
+        socket.to(`conversation:${conversationId}`).emit(
+            "conversation:typing:start",
+        {
+            userId,
+            conversationId
+        });
+    });
+    socket.on("conversation:typing:stop", ({ conversationId }: { conversationId: string }) => {
+        const userId = socket.data.user.id;
+        socket.to(`conversation:${conversationId}`).emit(
+            "conversation:typing:stop",
+            {
+                userId,
+                conversationId
+            }
+        )
+    })
+};
+
