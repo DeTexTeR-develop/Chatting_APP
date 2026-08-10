acfc42de071238bd3bbc812ea07aecd9b1414024
DeTexTeR-develop <ujjwalsingh90official@gmail.com>
2026-08-11 00:53:15 +0530

    containerized the server

--- server/src/services/redisService/redis.ts
+++ server/src/services/redisService/redis.ts
@@ -1,5 +1,9 @@
 import {Redis} from 'ioredis';
 
-const redisClient = new Redis();
+const redisClient = new Redis({
+    host: process.env.REDIS_HOST || 'localhost',
+    port: Number(process.env.REDIS_PORT) || 6379,
+});
+
 redisClient.on("error", (err) => console.error('Redis err: ', err));
 export default redisClient;
